3bdf94fbaa7347a10a947052a19013b4c6bd3db3
MichaelKremser <mkspamx-common@yahoo.de>
2015-02-10 22:05:29 +0100

    Extracted IFragment from Fragment

--- libtisiwebdll/DataStructures.cs
+++ libtisiwebdll/DataStructures.cs
@@ -3,8 +3,20 @@ using System.Collections.Generic;
 
 namespace mkcs.libtisiweb
 {
-	public class Fragment
+	public interface IFragment
 	{
+		string Name { get; set; }
+		FragmentSubsets Subsets { get; set; }
+		void AddSubset (string subset, string value);
+	}
+
+	public class Fragment : IFragment
+	{
+		public Fragment()
+		{
+			Subsets = new FragmentSubsets(this);
+		}
+
 		public string Name { get; set; }
 
 		public FragmentSubsets Subsets { get; set; }
@@ -13,15 +25,27 @@ namespace mkcs.libtisiweb
 		{
 			Subsets.Add (subset, value);
 		}
+
+		public override string ToString ()
+		{
+			return string.Format("[Fragment: Name={0}, Subsets={1}]", Name, Subsets.Count);
+		}
 	}
 
 	public class FragmentSubsets : Dictionary<string, string>
 	{
-		public Fragment OwningFragment { get; set; }
+		public FragmentSubsets() { }
+
+		public FragmentSubsets(IFragment OwningFragment) : this()
+		{
+			this.OwningFragment = OwningFragment;
+		}
+
+		public Fragment OwningFragment { get; private set; }
 
 		public override string ToString ()
 		{
-			return string.Format ("[FragmentSubsets]{0}{1}", (OwningFragment == null ? "" : OwningFragment), this.Count);
+			return string.Format ("[FragmentSubsets]{0}/{1}", (OwningFragment == null ? "" : OwningFragment), this.Count);
 		}
 	}
 }
