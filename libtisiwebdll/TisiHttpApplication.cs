9cb3f6d73cdabad6ab825e8c9e27b639a76f0909
MichaelKremser <mkspamx-common@yahoo.de>
2015-03-04 15:49:39 +0100

    Comments in TisiHttpApplication

--- libtisiwebdll/TisiHttpApplication.cs
+++ libtisiwebdll/TisiHttpApplication.cs
@@ -11,18 +11,32 @@ namespace mkcs.libtisiweb
 			FallbackSubset = "en";
 		}
 
-		private static string selectedSubset = "";
-
+		/// <summary>
+		/// Gets or sets the fallback subset that will be used in AutoDetectLanguage if no language could be detected.
+		/// </summary>
+		/// <value>The fallback subset.</value>
 		public string FallbackSubset {
 			get;
 			set;
 		}
+		
+		/// <summary>
+		/// The selected subset that will be used to retrieve texts.
+		/// </summary>
+		protected static string selectedSubset = "";
 
+		/// <summary>
+		/// Gets the currently selected subset.
+		/// </summary>
+		/// <returns>The selected subset.</returns>
 		public static string GetSelectedSubset()
 		{
 			return selectedSubset;
 		}
 
+		/// <summary>
+		/// Detects the language that the user has set in the browser and sets the selectedSubset appropriately. If no language can be detected, FallbackSubset will be used. Can be overridden in derived classes to implement a custom language detection mechanism.
+		/// </summary>
 		public void AutoDetectLanguage()
 		{
 			if (HttpContext.Current != null && HttpContext.Current.Request != null) {
