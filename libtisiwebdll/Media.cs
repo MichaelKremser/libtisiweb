515a0e8781c0063861bc5c8cb8e5c288740139fd
MichaelKremser <mkspamx-common@yahoo.de>
2015-10-02 21:05:48 +0200

    Added class Media and MediaType

---
+++ libtisiwebdll/Media.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace libtisiwebdll {
+
+	/// <summary>
+	/// Represents information about media like a picture, a video, an audio file or the like.
+	/// </summary>
+	public class Media {
+		public Media () {
+		}
+
+		public MediaType MediaType { get; set; }
+		public string ID { get; set; }
+		public DateTime Created { get; set; }
+		public DateTime Modified { get; set; }
+		public long Filesize { get; set; }
+		public int Rotation { get; set; }
+		public long Width { get; set; }
+		public long Height { get; set; }
+		public double Longitude { get; set; }
+		public double Latitude { get; set; }
+		public double Elevation { get; set; }
+		public string Author { get; set; }
+		public string Device { get; set; }
+		public string CustomData { get; set; }
+		public List<string> Tags { get; set; }
+	}
+}
+
---
+++ libtisiwebdll/MediaType.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace libtisiwebdll {
+
+	public enum MediaType {
+		Unknown = 0,
+		Picture = 1,
+		Video = 2,
+		Audio = 4
+	}
+}
+
